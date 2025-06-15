28c67ced893fcb04898b218c01f2f7edf50d47a6
Tarek-Salah-Taha <tareksalah168@gmail.com>
2025-06-15 08:55:57 +0300

    fix

--- app/_lib/supabase.ts
+++ app/_lib/supabase.ts
@@ -1,7 +1,14 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://jcnxjvrwruueplpsmdse.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpjbnhqdnJ3cnV1ZXBscHNtZHNlIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0ODI2Nzg1NCwiZXhwIjoyMDYzODQzODU0fQ.2G0aqQgl40OtluDXjCHvTMMNOtpAY7RopMaaARhUBr4";
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error(
+    "Missing Supabase environment variables: SUPABASE_URL or SUPABASE_KEY"
+  );
+}
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
