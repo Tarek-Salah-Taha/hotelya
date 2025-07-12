ad2dddbef0ed31dd07c2b4ee1446558d443ef937
Tarek-Salah-Taha <tareksalah168@gmail.com>
2025-07-12 15:15:13 +0300

    fix locale issue in url

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,48 @@ const nextConfig = {
   experimental: {
     turbo: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.ipwhois.io/flags/**",
+      },
+      {
+        protocol: "https",
+        hostname: "jcnxjvrwruueplpsmdse.supabase.co",
+        pathname: "/storage/v1/object/public/hotels/exterior/**",
+      },
+      {
+        protocol: "https",
+        hostname: "jcnxjvrwruueplpsmdse.supabase.co",
+        pathname: "/storage/v1/object/public/hotels/restaurants/**",
+      },
+      {
+        protocol: "https",
+        hostname: "jcnxjvrwruueplpsmdse.supabase.co",
+        pathname: "/storage/v1/object/public/hotels/rooms/**",
+      },
+      {
+        protocol: "https",
+        hostname: "jcnxjvrwruueplpsmdse.supabase.co",
+        pathname: "/storage/v1/object/public/rooms/types/**",
+      },
+      {
+        protocol: "https",
+        hostname: "jcnxjvrwruueplpsmdse.supabase.co",
+        pathname: "/storage/v1/object/public/avatars/avatars/**",
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/en",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
